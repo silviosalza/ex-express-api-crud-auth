4b6f4663aa33ae9453842c5424967a469fdca808
silviosalza <silviofilipposalza@gmail.com>
2023-11-27 20:31:44 +0100

    added auth middleware to post routes

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController.js");
 const {body, checkSchema} = require("express-validator")
-const postCreate = require("../validations/postCreate.js")
+const postCreate = require("../validations/postCreate.js");
+const authHandler = require("../middlewares/authHandler.js");
 
 
 
@@ -11,10 +12,10 @@ router.get("/", postController.index)
 //rotta show
 router.get("/:slug", postController.show)
 //rotta store
-router.post("/", checkSchema(postCreate),postController.store)
+router.post("/", checkSchema(postCreate), authHandler,postController.store)
 //rotta update
-router.put("/:slug", postController.update)
+router.put("/:slug",authHandler, postController.update)
 //rotta delete
-router.delete("/:slug", postController.destroy)
+router.delete("/:slug",authHandler, postController.destroy)
 
 module.exports = router;
